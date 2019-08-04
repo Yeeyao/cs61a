7c465c242c4bfc4f7e8686bc8b36a878582cecbe
huangyao <yaoli00@protonmai.com>
2019-08-04 22:06:32 +0800

    lab2 python code

--- lab2.py
+++ lab2.py
@@ -107,3 +107,68 @@ def count_cond(condition):
     8
     """
     "*** YOUR CODE HERE ***"
+    def satisfy_num(n):
+        i, count = 1, 0
+        while i <= n:
+            if condition(n, i):
+                count += 1
+            i += 1
+        return count
+    return satisfy_num
+
+def cycle(f1, f2, f3):
+    """Returns a function that is itself a higher-order function.
+
+    >>> def add1(x):
+    ...     return x + 1
+    >>> def times2(x):
+    ...     return x * 2
+    >>> def add3(x):
+    ...     return x + 3
+    >>> my_cycle = cycle(add1, times2, add3)
+    >>> identity = my_cycle(0)
+    >>> identity(5)
+    5
+    >>> add_one_then_double = my_cycle(2)
+    >>> add_one_then_double(1)
+    4
+    >>> do_all_functions = my_cycle(3)
+    >>> do_all_functions(2)
+    9
+    >>> do_more_than_a_cycle = my_cycle(4)
+    >>> do_more_than_a_cycle(2)
+    10
+    >>> do_two_cycles = my_cycle(6)
+    >>> do_two_cycles(1)
+    19
+    """
+    "*** YOUR CODE HERE ***"
+    def cycle_func(n):
+        def cycle_func2(result):
+            i = n
+            if i == 0:
+                return result
+            if i % 3 == 0:
+                return cycle_func2(f3(n))
+            if i % 3 == 1:
+                return cycle_func2(f1(n))
+            if i % 3 == 2:
+                return cycle_func2(f2(n))
+            i -= 1
+        return cycle_func2
+    return cycle_func
+
+    # def ret_func(n):
+    #     def ret(x):
+    #         i = 0
+    #         while i < n: 
+    #             if i % 3 == 0:
+    #                 x = f1(x)
+    #             elif i % 3 == 1:
+    #                 x = f2(x)
+    #             else:
+    #                 x = f3(x)
+    #             i += 1
+    #         return x
+    #     return ret
+    # return ret_func
